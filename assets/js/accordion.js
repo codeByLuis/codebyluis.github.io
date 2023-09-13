7298875e97a6be9a19a7952b3f62e762927e022d
Luis Aguilar <46345699+codeByLuis@users.noreply.github.com>
2023-09-13 16:51:14 -0700

    Add files via upload

---
+++ assets/js/accordion.js
@@ -0,0 +1,9 @@
+$( function() {
+    $( "#accordion" ).accordion({
+        collapsible: true,
+        icons: { "header": "ui-icon-alert", "activeHeader": "ui-icon-minus" }
+       
+    });
+  } );
+
+
